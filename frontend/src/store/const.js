e5263a3efe2a183ef423fa7de0f948f8b5e443ab
Salmiak <alfred@salmiakmedia.se>
2017-10-05 22:47:09 +0200

    Move API-related stuff to global file

---
+++ frontend/src/store/const.js
@@ -0,0 +1,1 @@
+export const apiUri = "//localhost:8888/mat/wp-json/wp/v2"
--- frontend/src/store/modules/recipes.js
+++ frontend/src/store/modules/recipes.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
-
-var apiUri = "//localhost:8888/mat/wp-json/wp/v2"
+import * as global from '../const'
 
 // initial state
 const state = {
@@ -21,7 +20,7 @@ const actions = {
   getAllRecipes ({ commit }) {
     var page = 1
     var requestPage = (page) => {
-      Vue.http.get(apiUri+'/recipe/?page='+page).then(response => {
+      Vue.http.get(global.apiUri+'/recipe/?page='+page).then(response => {
 
         if ( parseInt(response.headers.map['x-wp-totalpages'][0]) != page ) {
           page++
