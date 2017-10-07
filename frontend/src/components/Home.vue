dae517b4ce5e45e4dd1aa88f92f5eeb36813c8b7
Salmiak <alfred@salmiakmedia.se>
2017-10-08 00:13:22 +0200

    Dynamic title on week

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,5 +1,9 @@
 <template>
   <div id="home">
+    <h1 v-if="isThisWeek">Denna vecka</h1>
+    <h1 v-else-if="isThisNextWeek">Nästa vecka</h1>
+    <h1 v-else-if="isThisPrevWeek">Förra vecka</h1>
+    <h1 v-else>Vecka {{week}}</h1>
     <week-nav></week-nav>
     <week v-bind:year="year" v-bind:week="week"></week>
   </div>
@@ -14,15 +18,23 @@ export default {
   name: 'Home',
   components:{ WeekNav,Week },
   data() {
+    let year = this.$route.params.year ? parseInt(this.$route.params.year) :  moment().isoWeekYear()
+    let week = this.$route.params.week ? parseInt(this.$route.params.week) : moment().isoWeek()
     return {
-      year: this.$route.params.year ? parseInt(this.$route.params.year) :  moment().isoWeekYear(),
-      week: this.$route.params.week ? parseInt(this.$route.params.week) : moment().isoWeek()
+      year: year,
+      week: week,
+      isThisWeek: moment().isoWeek() == week && moment().isoWeekYear() == year,
+      isThisNextWeek: moment().add(1,'w').isoWeek() == week && moment().add(1,'w').isoWeekYear() == year,
+      isThisPrevWeek: moment().subtract(1,'w').isoWeek() == week && moment().subtract(1,'w').isoWeekYear() == year
     }
   },
   watch: {
     '$route' (to, from) {
       this.year = this.$route.params.year ? parseInt(this.$route.params.year) :  moment().isoWeekYear(),
       this.week = this.$route.params.week ? parseInt(this.$route.params.week) : moment().isoWeek()
+      this.isThisWeek = moment().isoWeek() == this.week && moment().isoWeekYear() == this.year
+      this.isThisNextWeek = moment().add(1,'w').isoWeek() == this.week && moment().add(1,'w').isoWeekYear() == this.year
+      this.isThisPrevWeek = moment().subtract(1,'w').isoWeek() == this.week && moment().subtract(1,'w').isoWeekYear() == this.year
     }
   }
 }
