4fad92357ee4f38d046edef6478adcc5acd2cc08
Salmiak <alfred@salmiakmedia.se>
2018-06-22 21:26:59 +0200

    MVEN tutorial result

---
+++ server/src/app.js
@@ -0,0 +1,112 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+// const MongoClient = require('mongodb').MongoClient
+var Post = require("../models/post");
+//var db
+
+/*
+MongoClient.connect('mongodb://mat-user:b68mclzReZqJnHksTq1D@ds161710.mlab.com:61710/mat', (err, client) => {
+  if (err) return console.log(err)
+
+  db = client.db('mat') // whatever your database name is
+  app.listen(process.env.PORT || 8081, () => {
+    console.log('listening on ' + (process.env.PORT || 8081))
+  })
+  // ... start the server
+})
+*/
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://mat-user:b68mclzReZqJnHksTq1D@ds161710.mlab.com:61710/mat');
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function(callback){
+  console.log("Connection Succeeded");
+  app.listen(process.env.PORT || 8081, () => {
+    console.log('listening on ' + (process.env.PORT || 8081))
+  })
+});
+
+
+
+
+// Creat new post
+app.post('/posts', (req, res) => {
+  var db = req.db;
+  var title = req.body.title;
+  var description = req.body.description;
+  var new_post = new Post({
+    title: title,
+    description: description
+  })
+
+  new_post.save(function (error) {
+    if (error) {
+      console.log(error)
+    }
+    res.send({
+      success: true,
+      message: 'Post saved successfully!'
+    })
+  })
+})
+
+// Read all posts
+app.get('/posts', (req, res) => {
+  Post.find({}, 'title description', function (error, posts) {
+    if (error) { console.error(error); }
+    res.send({
+      posts: posts
+    })
+  }).sort({_id:-1})
+})
+
+// Read single post
+app.get('/post/:id', (req, res) => {
+  var db = req.db;
+  Post.findById(req.params.id, 'title description', function (error, post) {
+    if (error) { console.error(error); }
+    res.send(post)
+  })
+})
+
+// Update a post
+app.put('/posts/:id', (req, res) => {
+  var db = req.db;
+  Post.findById(req.params.id, 'title description', function (error, post) {
+    if (error) { console.error(error); }
+
+    post.title = req.body.title
+    post.description = req.body.description
+    post.save(function (error) {
+      if (error) {
+        console.log(error)
+      }
+      res.send({
+        success: true
+      })
+    })
+  })
+})
+
+// Delete a post
+app.delete('/posts/:id', (req, res) => {
+  var db = req.db;
+  Post.remove({
+    _id: req.params.id
+  }, function(err, post){
+    if (err)
+      res.send(err)
+    res.send({
+      success: true
+    })
+  })
+})
