3dbcc4e0f1795d0b4117c929b940ccad06f84a3f
Salmiak <alfred@salmiakmedia.se>
2018-07-02 14:43:17 +0200

    Fixed bugg in Edit Meal

--- client/src/components/EditMeal.vue
+++ client/src/components/EditMeal.vue
@@ -51,7 +51,7 @@ export default {
     return {
       searchKeys: ['title', 'comment'],
       searchTerm: '',
-      recipeSearchResults: undefined
+      recipeSearchResults: []
     }
   },
   created () {
