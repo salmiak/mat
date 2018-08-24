292a41e08c1b4a317882456bb1c1e06d0cc8d7cb
Alfred Beckman <alfred@salmiakmedia.se>
2018-08-24 15:01:48 +0200

    Moved server to Ireland

--- client/config/prod.env.js
+++ client/config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_HOST: '"https://24md50p2t2.execute-api.us-east-1.amazonaws.com/dev/"'
+  API_HOST: '"https://w5sv2j9xl8.execute-api.eu-west-1.amazonaws.com/dev/"'
 }
